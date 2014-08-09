7775c17bb5434a9c58d9684127f6aaac15b31bf6
jpuyy-mba <jpuyy.com@gmail.com>
2014-08-09 10:46:05 +0800

    read eth and mac l2 address

--- parser.py
+++ parser.py
@@ -1,41 +1,46 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 # author jpuyy.com
+# read hardware infomation and update to racktables
 
 import json
-import csv
 import subprocess
+import urllib
+import urllib2
+import httplib
 
 subprocess.call("ansible -i hosts all -m setup --tree .",shell=True)
 
-with open('result.csv','wb') as csvfile:
-    serverwriter = csv.writer(csvfile, delimiter=',',quotechar='|',quoting=csv.QUOTE_MINIMAL)
-    serverwriter.writerow(['ip','型号','序列号','cpu','cpu个数','内存MB','硬盘大小','系统版本'])
-
-#with open('hosts', 'r') as f:
 for line in open('hosts', 'r').readlines():
     line = line.strip('\n')
     fopen = open(line,'r')
     
     output = json.loads(fopen.read())
     
-    print 'cpu'
-    print output['ansible_facts']['ansible_processor'][1]
-    
-    print '---------'
-    with open('result.csv','a') as csvfile:
-        serverwriter = csv.writer(csvfile, delimiter=',',quotechar='|',quoting=csv.QUOTE_MINIMAL)
-        #if output['failed']==true
-        #    jump
-        #serverwriter.writerow
-        serverwriter.writerow([
-            output['ansible_facts']['ansible_default_ipv4']['address'],
-            output['ansible_facts']['ansible_product_name'],
-            output['ansible_facts']['ansible_product_serial'],
-            output['ansible_facts']['ansible_processor'][1],
-            #output['ansible_facts']['ansible_processor_cores'],
-            output['ansible_facts']['ansible_processor_count'],
-            output['ansible_facts']['ansible_memtotal_mb'],
-            'nonono',
-            output['ansible_facts']['ansible_distribution']
-        ])
+    product_model = output['ansible_facts']['ansible_product_name']
+    serial = output['ansible_facts']['ansible_product_serial']
+    cpu_model = output['ansible_facts']['ansible_processor'][1]
+    cpu_num = output['ansible_facts']['ansible_processor_count']
+    memory = output['ansible_facts']['ansible_memtotal_mb']
+    hostname = output['ansible_facts']['ansible_fqdn']
+    #system_version = output['ansible_facts']['ansible_lsb']['description']
+    system_version = output['ansible_facts']['ansible_distribution_version']
+    eth = {}
+    mac = {}
+
+    # get eth and mac address
+    for interface in output['ansible_facts']['ansible_interfaces']:
+        if(interface != 'lo'):
+            int_key = interface.replace('_',':')
+            ansible_int = 'ansible_' + interface
+            if 'ipv4' in output['ansible_facts'][ansible_int]:
+                int_value = output['ansible_facts'][ansible_int]['ipv4']['address']
+                if(int_value):
+                    eth[int_key] = int_value
+            if 'macaddress' in output['ansible_facts'][ansible_int]:
+                mac_value = output['ansible_facts'][ansible_int]['macaddress'].replace(':','')
+                if(mac_value):
+                    mac[int_key] = mac_value
+
+    ethjson = json.dumps(eth)
+    macjson = json.dumps(mac)
